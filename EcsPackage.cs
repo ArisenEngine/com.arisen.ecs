2bb0b76cc69d33bdde50bae29b81068ad60a41d6
wjwen <wjwen@wooduan.com>
2026-03-28 10:03:55 +0800

    log

--- EcsPackage.cs
+++ EcsPackage.cs
@@ -1,6 +1,7 @@
 using ArisenKernel.Packages;
 using ArisenKernel.Services;
 using ArisenKernel.Contracts;
+using ArisenKernel.Diagnostics;
 using ArisenEngine.Core.ECS;
 using ArisenEngine.Core.Automation;
 
@@ -14,7 +15,7 @@ public class EcsPackage : IPackageEntry
     {
         m_EntityManager = new EntityManager();
         registry.RegisterService<IEntityManager>(m_EntityManager);
-        System.Console.WriteLine("[EcsPackage] Loaded and registered IEntityManager");
+        KernelLog.Info("[EcsPackage] Loaded and registered IEntityManager");
     }
 
     public void OnUnload(IServiceRegistry registry)
@@ -24,7 +25,7 @@ public class EcsPackage : IPackageEntry
             // Unregister functionality is not yet defined in the registry contract
             m_EntityManager = null;
         }
-        System.Console.WriteLine("[EcsPackage] Unloaded");
+        KernelLog.Info("[EcsPackage] Unloaded");
     }
 }
 
